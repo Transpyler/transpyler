9793b4269fc2bd31f5cd5a8e081a41234b5cbeae
fabiomacedomendes <fabiomacedomendes@gmail.com>
2017-03-28 04:57:20 -0300

    fix text that breaks 3.3 and 3.4 syntax

--- src/transpyler/tests/test_tool_google_translate.py
+++ src/transpyler/tests/test_tool_google_translate.py
@@ -1,30 +1,39 @@
+import sys
+
 from mock import mock
 
-from transpyler.tools.google_translate import translate_functions, translate_function, get_args
+from transpyler.tools.google_translate import translate_functions, \
+    translate_function, get_args
 
 
-def function(x, y=1, *args, z=1, d=2, **kwargs):
+def f(x, y=1, *args, z=1, d=2, **kwargs):
     """
     Some function!
     """
-    pass
+
+
+def g(x, y=1, *args, z=1, d=2, **kwargs):
+    """
+    Some other function!
+    """
 
 
 def test_get_correct_args():
-    args = get_args(function)
+    args = get_args(f)
     assert args == [('x', None), ('y', '1'), ('*args', None), ('z', '1'),
                     ('d', '2'), ('**kwargs', None)]
 
 
 def test_google_translate_function():
+    # Python 3.3 and 3.4 syntax breaks with complex g function.
+    func = f if sys.version_info[1] >= 5 else g
+
     with mock.patch('textblob.TextBlob.translate', lambda x, y, z: str(x)):
-        src = translate_function(function, 'pt-br')
-    print(src)
+        src = translate_function(func, 'pt-br')
     compile('\n' + src + '\n', '<input>', 'exec')
 
 
 def test_google_translate_builtin():
     with mock.patch('textblob.TextBlob.translate', lambda x, y, z: str(x)):
         src = translate_functions([dir], 'pt-br')[0]
-    print(src)
     compile(src, '<input>', 'exec')
