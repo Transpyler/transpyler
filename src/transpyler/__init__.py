88d8ffab7c30e201d2e55a558042c4dd9ca0495d
pwener <phwener@gmail.com>
2017-10-17 18:42:50 -0200

    Import runners to transpyler/__init__

--- src/transpyler/__init__.py
+++ src/transpyler/__init__.py
@@ -2,3 +2,4 @@ from .__meta__ import __author__, __version__
 from .errors import BadSyntaxError
 from .lexer import Lexer
 from .transpyler import Transpyler, get_transpyler
+from .runners import run
